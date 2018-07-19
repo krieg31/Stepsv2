faa20032fa9e15dcae7d95893265298c12f6918f
krieg31 <krieg31@users.noreply.github.com>
2018-07-19 10:39:03 +0900

    upd 1

--- app/src/main/java/com/example/stepsv2/MainActivity.java
+++ app/src/main/java/com/example/stepsv2/MainActivity.java
@@ -3,13 +3,24 @@ package com.example.stepsv2;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.stepsv2.fragments.main_frag;
+import com.example.stepsv2.fragments.second_frag;
+import com.example.stepsv2.fragments.settings_frag;
+import com.example.stepsv2.fragments.third_frag;
 
-    private TextView mTextMessage;
+public class MainActivity extends FragmentActivity {
+
+
+    main_frag main_frag = new main_frag();
+    second_frag second_frag = new second_frag();
+    third_frag third_frag = new third_frag();
+    settings_frag settings_frag = new settings_frag();
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -18,14 +29,17 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frgmCont, main_frag).commit();
+                    break;
+                case R.id.navigation_second:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frgmCont, second_frag).commit();
+                    break;
+                case R.id.navigation_third:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frgmCont, third_frag).commit();
+                    break;
+                case R.id.navigation_settings:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frgmCont, settings_frag).commit();
+                    break;
             }
             return false;
         }
@@ -36,7 +50,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }

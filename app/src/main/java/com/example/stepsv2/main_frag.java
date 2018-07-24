0b461c3ced332bc7cc03de631f9162174e15e1d9
bozhedon <bozhedon-yan@yandex.ru>
2018-07-24 14:23:46 +0900

    fix

--- app/src/main/java/com/example/stepsv2/main_frag.java
+++ app/src/main/java/com/example/stepsv2/main_frag.java
@@ -1,34 +1,16 @@
 package com.example.stepsv2;
 
-import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.content.Context;
-<<<<<<< HEAD
+import android.app.Fragment;
 import android.graphics.drawable.AnimationDrawable;
-import android.graphics.drawable.BitmapDrawable;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
-=======
-import android.content.Intent;
->>>>>>> GPS
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-//import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.app.Fragment;
-<<<<<<< HEAD
 import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.example.stepsv2.R;
-
-public class main_frag extends Fragment{
+//import android.support.v4.app.Fragment;
+public class main_frag extends Fragment {
 
     private AnimationDrawable mAnimationDrawable;
 
@@ -44,32 +26,7 @@ public class main_frag extends Fragment{
         mAnimationDrawable = (AnimationDrawable) imageView.getBackground();
         mAnimationDrawable.start();
         return inflater.inflate(R.layout.fragment_main_frag, container, false);
-=======
-import android.widget.Button;
 
 
-import com.example.stepsv2.R;
-
-public class main_frag extends Fragment {
-
-    public Button start;
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_main_frag,container, false);
-        Button button = view.findViewById(R.id.start);
-        button.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                Intent myIntent = new Intent(getActivity(), StartActivity.class);
-                getActivity().startActivity(myIntent);
-            }
-        });
-        return view;
->>>>>>> GPS
     }
-
-
 }

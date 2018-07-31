f37183b0b952814bc9d5afd551bd650077ebea93
krieg31 <krieg31@users.noreply.github.com>
2018-07-31 15:10:36 +0900

    Merge branch 'master' of https://github.com/krieg31/Stepsv2
    # Conflicts:
    #	app/src/main/java/com/example/stepsv2/StartActivity.java

--- app/src/main/java/com/example/stepsv2/MyAdapter.java
+++ app/src/main/java/com/example/stepsv2/MyAdapter.java
@@ -10,7 +10,7 @@ public class MyAdapter extends FragmentPagerAdapter {
     }
     @Override
     public int getCount() {
-        return(4);
+        return(3);
     }
     @Override
     public Fragment getItem(int position) {
--- app/src/main/java/com/example/stepsv2/daily.java
+++ app/src/main/java/com/example/stepsv2/daily.java
@@ -1,19 +1,20 @@
 package com.example.stepsv2;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.ValueDependentColor;
-import com.jjoe64.graphview.series.BarGraphSeries;
-import com.jjoe64.graphview.series.DataPoint;
-import com.jjoe64.graphview.series.LineGraphSeries;
+import org.eazegraph.lib.charts.BarChart;
+import org.eazegraph.lib.charts.StackedBarChart;
+import org.eazegraph.lib.charts.ValueLineChart;
+import org.eazegraph.lib.models.BarModel;
+import org.eazegraph.lib.models.StackedBarModel;
+import org.eazegraph.lib.models.ValueLinePoint;
+import org.eazegraph.lib.models.ValueLineSeries;
+
 
 public class daily extends Fragment {
 
@@ -39,69 +40,53 @@ public class daily extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+
         View view = inflater.inflate(R.layout.fragment_daily, container, false);
-        GraphView graph= view.findViewById(R.id.graph);
-        BarGraphSeries<DataPoint> series;
+        ValueLineChart mCubicValueLineChart = view.findViewById(R.id.cubiclinechart);
+        BarChart graph= view.findViewById(R.id.graph);
 
         switch (pageNumber){
             case 0:
-                 series= new BarGraphSeries<DataPoint>(new DataPoint[] {
-                        new DataPoint(0, 1),
-                        new DataPoint(0, 0),
-                });
-                seriescustom(series);
-                series.setTitle("1st");
-                graph.addSeries(series);
+                graph.setVisibility(View.VISIBLE);
+                mCubicValueLineChart.setVisibility(View.INVISIBLE);
+                graph.addBar(new BarModel(2.3f, 0xFF123456));
+                graph.addBar(new BarModel(2.f,  0xFF343456));
+                graph.addBar(new BarModel(3.3f, 0xFF563456));
+                graph.addBar(new BarModel(1.1f, 0xFF873F56));
+                graph.addBar(new BarModel(2.7f, 0xFF56B7F1));
+                graph.addBar(new BarModel(2.f,  0xFF343456));
+                graph.addBar(new BarModel(0.4f, 0xFF1FF4AC));
+                graph.addBar(new BarModel(4.f,  0xFF1BA4E6));
+                graph.startAnimation();
                 break;
             case 1:
-                series= new BarGraphSeries<DataPoint>(new DataPoint[] {
-                        new DataPoint(0, 1),
-                        new DataPoint(10, 10),
-                        new DataPoint(10, 10),
-                });
-                seriescustom(series);
-                series.setTitle("2nd");
-                graph.addSeries(series);
+                mCubicValueLineChart.setVisibility(View.VISIBLE);
+                graph.setVisibility(View.INVISIBLE);
+                ValueLineSeries series = new ValueLineSeries();
+                series.setColor(0xFF56B7F1);
+                series.addPoint(new ValueLinePoint("Jan", 0f));
+                series.addPoint(new ValueLinePoint("Jan", 2.4f));
+                series.addPoint(new ValueLinePoint("Feb", 3.4f));
+                series.addPoint(new ValueLinePoint("Mar", .4f));
+                series.addPoint(new ValueLinePoint("Apr", 1.2f));
+                series.addPoint(new ValueLinePoint("Mai", 2.6f));
+                series.addPoint(new ValueLinePoint("Jun", 1.0f));
+                series.addPoint(new ValueLinePoint("Jul", 3.5f));
+                series.addPoint(new ValueLinePoint("Aug", 2.4f));
+                series.addPoint(new ValueLinePoint("Sep", 2.4f));
+                series.addPoint(new ValueLinePoint("Oct", 3.4f));
+                series.addPoint(new ValueLinePoint("Nov", .4f));
+                series.addPoint(new ValueLinePoint("Dec", 1.3f));
+                series.addPoint(new ValueLinePoint("Jan", 0f));
+
+                mCubicValueLineChart.addSeries(series);
+                mCubicValueLineChart.startAnimation();
                 break;
             case 2:
-                series= new BarGraphSeries<DataPoint>(new DataPoint[] {
-                        new DataPoint(0, 1),
-                        new DataPoint(20, 20),
-                        new DataPoint(20, 20),
-                        new DataPoint(20, 20),
-                });
-                seriescustom(series);
-                series.setTitle("3rd");
-                graph.addSeries(series);
-                break;
-            case 3:
-                series= new BarGraphSeries<DataPoint>(new DataPoint[] {
-                        new DataPoint(0, 1),
-                        new DataPoint(40, 40),
-                        new DataPoint(40, 40),
-                        new DataPoint(40, 40),
-                        new DataPoint(40, 40),
-                });
-                seriescustom(series);
-                series.setTitle("4th");
-                graph.addSeries(series);
+                graph.setVisibility(View.INVISIBLE);
+                mCubicValueLineChart.setVisibility(View.INVISIBLE);
                 break;
         }
         return view;
     }
-
-
-    void seriescustom(BarGraphSeries series)
-    {
-        series.setSpacing(50);
-        series.setDrawValuesOnTop(true);
-        series.setValueDependentColor(new ValueDependentColor<DataPoint>() {
-            @Override
-            public int get(DataPoint data) {
-                return Color.rgb((int) data.getX()*255/4, (int) Math.abs(data.getY()*255/6), 100);
-            }
-        });
-    }
-
-
 }

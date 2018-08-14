12589cc642656a694e6a0d8e7ca99f5476cdd94e
krieg31 <krieg31@users.noreply.github.com>
2018-08-14 17:13:06 +0900

    Merge branch 'master' of https://github.com/krieg31/Stepsv2
    # Conflicts:
    #	app/src/main/res/layout/fragment_main_frag.xml
    #	app/src/main/res/values/colors.xml

---
+++ app/src/main/java/com/example/stepsv2/AppConfig.java
@@ -0,0 +1,9 @@
+package com.example.stepsv2;
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://u234765589.hostingerapp.com/android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://u234765589.hostingerapp.com/android_login_api/register.php";
+}
